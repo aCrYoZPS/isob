9b43d7c110ac52e973a87e2154815acfead0e8b0
aCrYoZ <acryozps@gmail.com>
2026-02-04 02:04:57 +0300

    first lab done + lab 2 caesar

--- lab2/main.py
+++ lab2/main.py
@@ -1,30 +1,66 @@
 class Caesar:
+    alphabets: list[str] = [
+        "abcdefghijklmnopqrstuvwxyz",
+        "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    ]
+
     @classmethod
-    def chipher(cls, plaintext:str, key: int) -> str:
+    def __inner_chipher(cls, text: str, key: int, mode: str = "chipher"):
         result = ""
-        for c in plaintext:
-            result += chr((ord(c) + key))
-        
+        if mode == "dechipher":
+            key *= -1
+
+        for c in text:
+            found = False
+            for alphabet in cls.alphabets:
+                alphabet_lower = alphabet.lower()
+                alphabet_upper = alphabet.upper()
+                if c in alphabet_lower:
+                    idx = alphabet_lower.index(c)
+                    new_idx = (idx + key) % len(alphabet_lower)
+                    result += alphabet_lower[new_idx]
+                    found = True
+                    break
+
+                elif c in alphabet_upper:
+                    idx = alphabet_upper.index(c)
+                    new_idx = (idx + key) % len(alphabet_upper)
+                    result += alphabet_upper[new_idx]
+                    found = True
+                    break
+
+            if not found:
+                result += c
+
         return result
+        pass
+
+    @classmethod
+    def chipher(cls, plaintext: str, key: int) -> str:
+        return cls.__inner_chipher(plaintext, key)
 
     @classmethod
     def dechipher(cls, chiphered_text: str, key: int) -> str:
-        result = ""
-        for c in chiphered_text:
-            result += chr(ord(c) - key)
-        
-        return result
+        return cls.__inner_chipher(chiphered_text, key, "dechipher")
+
 
 class Visener:
     @classmethod
-    def chipher(cls, plaintext: str, key:str) -> str:
+    def chipher(cls, plaintext: str, key: str) -> str:
         pass
 
-    def dechipher(cls, chiphered_text: str, key:str) -> str:
+    def dechipher(cls, chiphered_text: str, key: str) -> str:
         pass
+
 
 def main():
-    pass
+    p_t = "эЁюя aBc"
+    key = 1
+    c_t = Caesar.chipher(p_t, key)
+    n_p_t = Caesar.dechipher(c_t, key)
+    print(c_t)
+    print(n_p_t)
+
 
 if __name__ == "__main__":
     main()
